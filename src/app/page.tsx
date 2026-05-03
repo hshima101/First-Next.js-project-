2adb9cb32deb3de13edfcf1a13a7bd8608e4637b
Hideo Shima <hshima101@gmail.com>
2026-05-02 23:59:43 -0700

    Added header navigation icon and functions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main id="main">
       <Hero />
-      <h1>And uhh they talk about me jacking off to niki haily, I'm gonna jack off on your casket you cock sucking son of a bish.</h1>
+        <h3>And uhh they talk about me jacking off to niki haily, I'm gonna jack off on your casket you cock sucking son of a bish.</h3>
       <Posts />
 
     </main>
